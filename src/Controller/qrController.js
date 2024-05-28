243ac274b6c46b2468ce4df0d2307e15de6f49a9
sicalo330 <noah.esteban9@gmail.com>
2024-05-28 10:51:23 -0500

    Imprimir qr

--- src/Controller/qrController.js
+++ src/Controller/qrController.js
@@ -1,23 +1,64 @@
-import qr from 'qr-image'
-import path from 'path'
-import os from 'os'
-import fs from 'fs'
+import qr from 'qr-image';
+import path from 'path';
+import os from 'os';
+import fs from 'fs';
+import printer from 'pdf-to-printer';
+import PDFDocument from 'pdfkit';
 
 export const generateQR = async (req, res) => {
-    let { post } = req.body
+    try {
+        const { post } = req.body;
 
-    const codigoQR = qr.image(post, { type: 'png' });
+        // Crear el documento PDF
+        const doc = new PDFDocument();
+        
+        // Generar el código QR como imagen PNG
+        const codigoQR = qr.image(post, { type: 'png' });
 
-    // Ruta al escritorio del usuario
-    const escritorio = path.join(os.homedir(), 'Desktop');
+        // Ruta al escritorio del usuario
+        const escritorio = path.join(os.homedir(), 'Desktop');
 
-    const timestamp = new Date().getTime();
-    const nombreArchivo = path.join(escritorio, `codigo-qr-${timestamp}.png`);
-    codigoQR.pipe(fs.createWriteStream(nombreArchivo));
+        // Generar un nombre de archivo único basado en un timestamp
+        const timestamp = new Date().getTime();
+        const nombreArchivo = path.join(escritorio, `codigo-qr-${timestamp}.png`);
+        
+        // Guardar la imagen QR en el escritorio
+        const writeStream = fs.createWriteStream(nombreArchivo);
+        codigoQR.pipe(writeStream);
 
-    
-    // Enviar el archivo como descarga al navegador
-    res.download(nombreArchivo, () => {
-        console.log("Descarga exitosa")
-    });
+        // Esperar a que se termine de escribir la imagen antes de proceder
+        await new Promise((resolve, reject) => {
+            writeStream.on('finish', resolve);
+            writeStream.on('error', reject);
+        });
+
+        // Guardar el PDF en el escritorio
+        const pdfPath = path.join(escritorio, 'output.pdf');
+        doc.pipe(fs.createWriteStream(pdfPath));
+
+        // Añadir la imagen QR al PDF
+        doc.image(nombreArchivo, {
+            fit: [250, 250],
+            align: 'center',
+            valign: 'center'
+        });
+        doc.end();
+
+        // Esperar a que se termine de escribir el PDF antes de proceder
+        await new Promise((resolve, reject) => {
+            doc.on('end', resolve);
+            doc.on('error', reject);
+        });
+
+        // Enviar el archivo QR como descarga al navegador
+        await res.download(nombreArchivo, () => {
+            console.log("Descarga exitosa");
+        });
+
+        // Imprimir el PDF
+        await printer.print(pdfPath, { printer: 'ZDX' });
+        console.log("Conexión exitosa");
+    } catch (error) {
+        console.error(error);
+    }
 };
